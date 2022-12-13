2298dd4f1b02d02e89595b9e5d6bb6519482d21c
VincentGichohi <vincentegichohi@gmail.com>
2022-12-13 10:05:52 +0300

    add a middleware file

---
+++ accounts/middleware.py
@@ -0,0 +1,38 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+class AccountCheckMiddleware(MiddlewareMixin):
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        modulename = view_func.__module__
+        user = request.user   # get the current user
+        if user.is_authenticated:  # check if the user is authenticated
+            if user.user_type == '1':   # admin
+                if modulename == 'voting_views':  # granting access of views to admin
+                    error = True
+                    if request.path == reverse('fetch_ballot'):
+                        pass
+                    else:
+                        messages.error(
+                            request, "You do not have access to this resource")
+                        return redirect(reverse('adminDashboard'))
+            elif user.user_type == '2':   # voter
+                if modulename == 'administrator.views':
+                    messages.error(
+                        request, "You do not have access to this resource")
+                    return redirect(reverse('voterDashboard'))
+            else:
+                return redirect(reverse('account_login'))
+        else:
+            # if the path is login or has anything to do with authentication, pass
+            if request.path == reverse('account_login') or request.path == reverse('account_register') or modulename == 'django.contrib.auth.views' or request.path == reverse('account_login'):
+                pass
+            elif modulename == 'administrator.views' or modulename == 'voting.views':
+                # if visitor tries to access administrator or voters functions
+                messages.error(
+                    request, "You need to be logged in to perform this operation")
+                return redirect(reverse('account_login'))
+            else:
+                return redirect(reverse('account_login'))
