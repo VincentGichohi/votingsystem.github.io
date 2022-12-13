9c2d292ddbb24fd0d118c70fc3b6201fcfeca1c9
VincentGichohi <vincentegichohi@gmail.com>
2022-12-13 10:12:23 +0300

    add a voting app and a context processors file

---
+++ voting/context_processors.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+
+def ElectionTitle(request):
+    context = {}
+    title = "No title yet"
+    try:
+        file = open(settings.ELECTION_TITLE_PATH, 'r')
+        title = file.read()
+    except:
+        pass
+
+    context['TITLE'] = title
+    return context
