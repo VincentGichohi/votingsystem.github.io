fddbe02166064347dbd7ac9f326f45acfd64e914
VincentGichohi <vincentegichohi@gmail.com>
2022-12-13 10:12:42 +0300

    add a voting app and a forms file

---
+++ voting/forms.py
@@ -0,0 +1,22 @@
+from re import L
+from django import forms
+from .models import *
+from account.forms import FormSettings
+
+
+class VoterForm(FormSettings):
+    class Meta:
+        model = Voter
+        fields = ['phone']
+
+
+class PositionForm(FormSettings):
+    class Meta:
+        model = Position
+        fields = ['name', 'max_vote']
+
+
+class CandidateForm(FormSettings):
+    class Meta:
+        model = Candidate
+        fields = ['fullname', 'bio', 'position', 'photo']
